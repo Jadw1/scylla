scylladb/scylladb

service/raft/group0_state_machine: update service levels cache
Reload service levels cache when applying raft command.

Applying `service_level_change` reloads both service level caches.
To observe roles hierarchy and attributes changes, table_ids of
mutations of `write_mutations` are checked. Change to any of this table
reloads only effective service levels cache.

service/storage_service.cc
@@ -841,9 +841,9 @@ future<> storage_service::merge_topology_snapshot(raft_snapshot snp) {
     co_await _db.local().apply(freeze(muts), db::no_timeout);
 }
 
-future<> storage_service::update_service_levels_configuration() {
+future<> storage_service::update_service_levels_configuration(qos::update_only_effective_cache update_only_effective_cache) {
     assert(this_shard_id() == 0);
-    co_await _sl_controller.local().update_service_levels_cache();
+    co_await _sl_controller.local().update_cache(update_level);
 }
 
 // Moves the coroutine lambda onto the heap and extends its

service/storage_service.hh
@@ -867,7 +867,7 @@ public:
 
     // Reload service levels in-memory configuration once.
     // Must be called on shard 0.
-    future<> update_service_levels_configuration();
+    future<> update_service_levels_configuration(qos::update_only_effective_cache update_only_effective_cache = qos::update_only_effective_cache::no);
 
     future<> do_cluster_cleanup();
 

Jadw1/scylla

service/storage_service.cc
@@ -872,9 +872,9 @@ future<> storage_service::merge_topology_snapshot(raft_snapshot snp) {
     co_await _db.local().apply(freeze(muts), db::no_timeout);
 }
 
-future<> storage_service::update_service_levels_cache() {
+future<> storage_service::update_service_levels_cache(qos::update_only_effective_cache update_only_effective_cache) {
     assert(this_shard_id() == 0);
-    co_await _sl_controller.local().update_service_levels_cache();
+    co_await _sl_controller.local().update_cache(update_only_effective_cache);
 }
 
 // Moves the coroutine lambda onto the heap and extends its

service/storage_service.hh
@@ -884,7 +884,7 @@ public:
 
     // Reload service levels in-memory configuration once.
     // Must be called on shard 0.
-    future<> update_service_levels_cache();
+    future<> update_service_levels_cache(qos::update_only_effective_cache update_only_effective_cache = qos::update_only_effective_cache::no);
 
     future<> do_cluster_cleanup();
 