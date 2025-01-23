716db833215ed83a039d0f7b775cc7b1f7f5109f
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-01-23 12:00:21 +0100

    service/storage_service: run view building coordinator
    Run view building coordinator alongside topology coordinator.
    Ignore tablet-based views in view_builder.

--- db/view/view.cc
+++ db/view/view.cc
@@ -2479,6 +2479,11 @@ static future<> flush_base(lw_shared_ptr<replica::column_family> base, abort_sou
 }
 
 void view_builder::on_create_view(const sstring& ks_name, const sstring& view_name) {
+    if (_db.features().view_building_coordinator && _db.find_keyspace(ks_name).uses_tablets()) {
+        // skip tablets-based views in view_builder
+        return;
+    }
+
     // Do it in the background, serialized.
     (void)with_semaphore(_sem, 1, [ks_name, view_name, this] {
         auto view = view_ptr(_db.find_schema(ks_name, view_name));
@@ -2504,6 +2509,11 @@ void view_builder::on_create_view(const sstring& ks_name, const sstring& view_na
 }
 
 void view_builder::on_update_view(const sstring& ks_name, const sstring& view_name, bool) {
+    if (_db.features().view_building_coordinator && _db.find_keyspace(ks_name).uses_tablets()) {
+        // skip tablets-based views in view_builder
+        return;
+    }
+
     // Do it in the background, serialized.
     (void)with_semaphore(_sem, 1, [ks_name, view_name, this] {
         auto view = view_ptr(_db.find_schema(ks_name, view_name));
@@ -2521,6 +2531,11 @@ void view_builder::on_update_view(const sstring& ks_name, const sstring& view_na
 }
 
 void view_builder::on_drop_view(const sstring& ks_name, const sstring& view_name) {
+    if (_db.features().view_building_coordinator && _db.find_keyspace(ks_name).uses_tablets()) {
+        // skip tablets-based views in view_builder
+        return;
+    }
+    
     vlogger.info0("Stopping to build view {}.{}", ks_name, view_name);
     // Do it in the background, serialized.
     (void)with_semaphore(_sem, 1, [ks_name, view_name, this] {
