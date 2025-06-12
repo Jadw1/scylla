scylladb/scylladb

service/view_building_state: extract common helper methods
Extract common helper methods for grouping tasks by tablet_id, so they
can be used in other places.

service/view_building_coordinator.cc
@@ -380,19 +380,6 @@ std::vector<utils::UUID> view_building_coordinator::get_started_tasks(locator::t
     }) | std::ranges::to<std::vector>();
 }
 
-static std::map<locator::tablet_id, std::vector<view_building_task>> collect_tasks_by_tablet_id(const replica_tasks& replica_tasks) {
-    std::map<locator::tablet_id, std::vector<view_building_task>> tasks;
-    for (auto& [_, view_tasks]: replica_tasks.view_tasks) {
-        for (auto& [_, task]: view_tasks) {
-            tasks[task.tid].push_back(task);
-        }
-    }
-    for (auto& [_, task]: replica_tasks.staging_tasks) {
-        tasks[task.tid].push_back(task);
-    }
-    return tasks;
-}
-
 // Returns list of tasks which can be started together.
 // A task can be started if corresponding tablet is not in migration process.
 // All returned tasks can be executed together, meaning that their type, base_id, table_id and replica are the same.
@@ -416,7 +403,7 @@ std::vector<utils::UUID> view_building_coordinator::select_tasks_for_replica(loc
     }
 
     auto& tablet_map = _db.get_token_metadata().tablets().get_tablet_map(*_vb_sm.building_state.currently_processed_base_table);
-    for (auto& [tid, tasks]: collect_tasks_by_tablet_id(base_tasks[replica])) {
+    for (auto& [tid, tasks]: _vb_sm.building_state.collect_tasks_by_tablet_id(*_vb_sm.building_state.currently_processed_base_table, replica)) {
         if (tablet_map.get_tablet_transition_info(tid)) {
             vbc_logger.debug("Tablet {} on replica {} is in transition.", tid, replica);
             continue;

service/view_building_state.cc
@@ -134,6 +134,44 @@ std::vector<std::reference_wrapper<const view_building_task>> view_building_stat
     return host_tasks;
 }
 
+std::map<locator::tablet_id, std::vector<view_building_task>> view_building_state::collect_tasks_by_tablet_id(table_id base_table_id) const {
+    if (!tasks_state.contains(base_table_id)) {
+        return {};
+    }
+
+    std::map<locator::tablet_id, std::vector<view_building_task>> map;
+    auto merge_maps = [&] (std::map<locator::tablet_id, std::vector<view_building_task>>&& other) {
+        for (auto&& [tid, tasks]: std::move(other)) {
+            auto& tasks_vec = map[tid];
+            tasks_vec.insert(tasks_vec.end(), std::make_move_iterator(tasks.begin()), std::make_move_iterator(tasks.end()));
+        }
+    };
+
+    for (auto& [replica, _]: tasks_state.at(base_table_id)) {
+        merge_maps(collect_tasks_by_tablet_id(base_table_id, replica));
+    }
+
+    return map;
+}
+
+std::map<locator::tablet_id, std::vector<view_building_task>> view_building_state::collect_tasks_by_tablet_id(table_id base_table_id, const locator::tablet_replica& replica) const {
+    if (!tasks_state.contains(base_table_id) || !tasks_state.at(base_table_id).contains(replica)) {
+        return {};
+    }
+    
+    std::map<locator::tablet_id, std::vector<view_building_task>> tasks;
+    auto& replica_tasks = tasks_state.at(base_table_id).at(replica);
+    for (auto& [_, view_tasks]: replica_tasks.view_tasks) {
+        for (auto& [_, task]: view_tasks) {
+            tasks[task.tid].push_back(task);
+        }
+    }
+    for (auto& [_, task]: replica_tasks.staging_tasks) {
+        tasks[task.tid].push_back(task);
+    }
+    return tasks;
+}
+
 }
 
 }

service/view_building_state.hh
@@ -82,6 +82,8 @@ struct view_building_state {
     
     std::optional<std::reference_wrapper<const view_building_task>> get_task(table_id base_id, locator::tablet_replica replica, utils::UUID id) const;
     std::vector<std::reference_wrapper<const view_building_task>> get_tasks_for_host(table_id base_id, locator::host_id host) const;
+    std::map<locator::tablet_id, std::vector<view_building_task>> collect_tasks_by_tablet_id(table_id base_table_id) const;
+    std::map<locator::tablet_id, std::vector<view_building_task>> collect_tasks_by_tablet_id(table_id base_table_id, const locator::tablet_replica& replica) const;
 };
 
 // Represents global state of tablet-based views.

Jadw1/scylla

service/view_building_coordinator.cc
@@ -380,19 +380,6 @@ std::vector<utils::UUID> view_building_coordinator::get_started_tasks(locator::t
     }) | std::ranges::to<std::vector>();
 }
 
-static std::map<dht::token, std::vector<view_building_task>> collect_tasks_by_last_token(const replica_tasks& replica_tasks) {
-    std::map<dht::token, std::vector<view_building_task>> tasks;
-    for (auto& [_, view_tasks]: replica_tasks.view_tasks) {
-        for (auto& [_, task]: view_tasks) {
-            tasks[task.last_token].push_back(task);
-        }
-    }
-    for (auto& [_, task]: replica_tasks.staging_tasks) {
-        tasks[task.last_token].push_back(task);
-    }
-    return tasks;
-}
-
 // Returns list of tasks which can be started together.
 // A task can be started if corresponding tablet is not in migration process.
 // All returned tasks can be executed together, meaning that their type, base_id, table_id and replica are the same.
@@ -416,7 +403,7 @@ std::vector<utils::UUID> view_building_coordinator::select_tasks_for_replica(loc
     }
 
     auto& tablet_map = _db.get_token_metadata().tablets().get_tablet_map(*_vb_sm.building_state.currently_processed_base_table);
-    for (auto& [token, tasks]: collect_tasks_by_last_token(base_tasks[replica])) {
+    for (auto& [token, tasks]: _vb_sm.building_state.collect_tasks_by_last_token(*_vb_sm.building_state.currently_processed_base_table, replica)) {
         auto tid = tablet_map.get_tablet_id(token);
         if (tablet_map.get_tablet_transition_info(tid)) {
             vbc_logger.debug("Tablet {} on replica {} is in transition.", tid, replica);

service/view_building_state.cc
@@ -112,6 +112,44 @@ std::vector<std::reference_wrapper<const view_building_task>> view_building_stat
     return host_tasks;
 }
 
+std::map<dht::token, std::vector<view_building_task>> view_building_state::collect_tasks_by_last_token(table_id base_table_id) const {
+    if (!tasks_state.contains(base_table_id)) {
+        return {};
+    }
+
+    std::map<dht::token, std::vector<view_building_task>> map;
+    auto merge_maps = [&] (std::map<dht::token, std::vector<view_building_task>>&& other) {
+        for (auto&& [token, tasks]: std::move(other)) {
+            auto& tasks_vec = map[token];
+            tasks_vec.insert(tasks_vec.end(), std::make_move_iterator(tasks.begin()), std::make_move_iterator(tasks.end()));
+        }
+    };
+
+    for (auto& [replica, _]: tasks_state.at(base_table_id)) {
+        merge_maps(collect_tasks_by_last_token(base_table_id, replica));
+    }
+
+    return map;
+}
+
+std::map<dht::token, std::vector<view_building_task>> view_building_state::collect_tasks_by_last_token(table_id base_table_id, const locator::tablet_replica& replica) const {
+    if (!tasks_state.contains(base_table_id) || !tasks_state.at(base_table_id).contains(replica)) {
+        return {};
+    }
+    
+    std::map<dht::token, std::vector<view_building_task>> tasks;
+    auto& replica_tasks = tasks_state.at(base_table_id).at(replica);
+    for (auto& [_, view_tasks]: replica_tasks.view_tasks) {
+        for (auto& [_, task]: view_tasks) {
+            tasks[task.last_token].push_back(task);
+        }
+    }
+    for (auto& [_, task]: replica_tasks.staging_tasks) {
+        tasks[task.last_token].push_back(task);
+    }
+    return tasks;
+}
+
 }
 
 }

service/view_building_state.hh
@@ -80,6 +80,8 @@ struct view_building_state {
     
     std::optional<std::reference_wrapper<const view_building_task>> get_task(table_id base_id, locator::tablet_replica replica, utils::UUID id) const;
     std::vector<std::reference_wrapper<const view_building_task>> get_tasks_for_host(table_id base_id, locator::host_id host) const;
+    std::map<dht::token, std::vector<view_building_task>> collect_tasks_by_last_token(table_id base_table_id) const;
+    std::map<dht::token, std::vector<view_building_task>> collect_tasks_by_last_token(table_id base_table_id, const locator::tablet_replica& replica) const;
 };
 
 // Represents global state of tablet-based views.