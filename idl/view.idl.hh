scylladb/scylladb

message/messaging_service: add `work_on_view_building_tasks` RPC
The RPC will be used by view building coordinator to attach to and wait
for tasks performed by view building worker (introduced in later
commit).

The RPC gets vector of tasks' ids and returns vector of
`view_task_result`s.
i-th task result reffers to i-th task id.

idl/view.idl.hh
@@ -6,6 +6,8 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
+#include "idl/uuid.idl.hh"
+
 namespace db {
 namespace view {
 class update_backlog {
@@ -14,3 +16,17 @@ class update_backlog {
 };
 }
 }
+
+namespace service {
+namespace view_building {
+struct view_task_result {
+    enum class command_status: uint8_t {
+        fail,
+        success,
+    };
+    service::view_building::view_task_result::command_status status;
+};
+}
+}
+
+verb [[cancellable]] work_on_view_building_tasks(std::vector<utils::UUID> tasks_ids) -> std::vector<service::view_building::view_task_result>

Jadw1/scylla

idl/view.idl.hh
@@ -6,11 +6,24 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
+#include "idl/uuid.idl.hh"
+
 namespace db {
 namespace view {
 class update_backlog {
     size_t get_current_bytes();
     size_t get_max_bytes();
 };
+
+struct view_task_result {
+    enum class command_status: uint8_t {
+        success,
+        abort,
+    };
+    db::view::view_task_result::command_status status;
+};
+
 }
 }
+
+verb [[cancellable]] work_on_view_building_tasks(std::vector<utils::UUID> tasks_ids) -> std::vector<db::view::view_task_result>